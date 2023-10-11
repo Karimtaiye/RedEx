802688beb478e644ce4735f21b184951601ad075
Karimtaiye <olayinkahassan117@gmail.com>
2023-10-11 14:04:34 +0100

    ll

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import './Header.css'
+import './HeaderRes.css'
 import Logo from '../../assets/Logo1.png'
 import { NavLink, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { BiSolidCart } from 'react-icons/bi'
+import { RiMenu2Line } from 'react-icons/ri'
 import axios from 'axios'
 import { motion } from 'framer-motion'
 
@@ -62,28 +64,30 @@ function Header({home, about, blog, cart, login, renders}) {
       </div>
       <div className='RedExHeader_Nav'>
         <ul>
-         <NavLink to={'/'} style={{color:"black", textDecoration:"none", background:"none"}}>
+         <NavLink className="menu" to={'/'} style={{color:"black", textDecoration:"none", background:"none"}}>
               <li style={{color:home}}>Home</li>
          </NavLink>
-          <NavLink to={'/about'} style={{color:"black", textDecoration:"none", background:"none"}} >
+          <NavLink className="menu" to={'/about'} style={{color:"black", textDecoration:"none", background:"none"}} >
               <li style={{color:about}}>About us</li>
           </NavLink>
-          <NavLink to={'/blog'} style={{color:"black", textDecoration:"none", background:"none"}} >
+          <NavLink className="menu" to={'/blog'} style={{color:"black", textDecoration:"none", background:"none"}} >
               <li style={{color:blog}}>Blog</li>
           </NavLink>
-          <NavLink to={'/cart'} style={{color:"black", textDecoration:"none", background:"none"}} >
+          <NavLink className="menu" to={'/cart'} style={{color:"black", textDecoration:"none", background:"none"}} >
              <li style={{color:cart}}><BiSolidCart  style={{fontSize:'25px'}}/>
              <div className='Cart_Qty' style={{ background:cart}}>{userCart.length === 0?"...":userCart.items.length}</div>
              </li>
           </NavLink>
+          <li className='Burger' ><RiMenu2Line className='Menu_Icon'/> </li>
          {
           !token?
           <NavLink to={'/login'} style={{color:"black", textDecoration:"none", background:"none"}} >
              <li style={{color:login}}>Log in</li>
           </NavLink>:
-          <li onMouseOver={()=>{
+          <li  onMouseOver={()=>{
             setAccPopUp(!accPopUp)
-          }}  className='Profile' style={{fontFamily:"cursive", textDecoration:"none"}}>{firstname.charAt(0).toUpperCase() + lastname.charAt(0).toUpperCase()}</li>
+          }}  className='Profile' style={{fontFamily:"cursive", textDecoration:"none"}}>{firstname.charAt(0).toUpperCase() + lastname.charAt(0).toUpperCase()}
+          </li>
           
          }
         </ul>
